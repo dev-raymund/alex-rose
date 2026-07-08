3229c072a9e081744e23aa39a5cf33d60b013222
dev-raymund <raymundhermoso.dev@gmail.com>
2026-07-08 17:19:46 +0800

    FIX: Removed excerpt

--- wp-content/themes/alex-rose-2026/template-parts/off-the-cuff/single-content.php
+++ wp-content/themes/alex-rose-2026/template-parts/off-the-cuff/single-content.php
@@ -9,14 +9,9 @@ if (! defined('ABSPATH')) {
 	exit;
 }
 
-$excerpt = get_the_excerpt();
 ?>
 <section class="otca-content">
 	<div class="otca-content__inner ar-container ar-container--3xl">
-		<?php if ($excerpt) : ?>
-			<p class="otca-content__standfirst"><?php echo esc_html($excerpt); ?></p>
-		<?php endif; ?>
-
 		<div class="otca-content__prose entry-content">
 			<?php the_content(); ?>
 		</div>
